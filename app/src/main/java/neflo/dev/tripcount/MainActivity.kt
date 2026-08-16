50c5eff4fa14d21c5c43dcd817231f57ded11b1c
Alejandro Nebot <neflodeveloper@gmail.com>
2026-08-16 22:06:53 +0200

    build: start first screen basic layout

--- app/src/main/java/neflo/dev/tripcount/MainActivity.kt
+++ app/src/main/java/neflo/dev/tripcount/MainActivity.kt
@@ -1,47 +1,82 @@
 package neflo.dev.tripcount
 
+import android.annotation.SuppressLint
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import neflo.dev.tripcount.ui.theme.TripCountTheme
 
 class MainActivity : ComponentActivity() {
+
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TripCountTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold (
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = { CustomTopBar() },
+                    floatingActionButton = {
+                        FloatingActionButton(
+                            onClick = {},
+                            modifier = Modifier.padding(8.dp)
+                        ) {
+                            Icon(
+                                modifier = Modifier.size(36.dp),
+                                contentDescription = "addIcon",
+                                painter = painterResource(R.drawable.ic_add_short),
+                            )
+                        }
+                    }
+                ) { _ ->
+
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TripCountTheme {
-        Greeting("Android")
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun CustomTopBar() {
+        CenterAlignedTopAppBar(
+            title = {
+                Text(
+                    "TripCount",
+                    textAlign = TextAlign.Center
+                )
+            },
+            actions = {
+                IconButton(
+                    onClick = {},
+                    modifier = Modifier.padding(end = 16.dp)
+                ) {
+                    Icon(
+                        modifier = Modifier.size(36.dp),
+                        contentDescription = "profileIcon",
+                        painter = painterResource(R.drawable.ic_account),
+                    )
+                }
+            }
+        )
     }
+
 }
